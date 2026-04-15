2b74f37c06a1d7a45e35d32b8395f300f68d4b2c
ASOST\2416071 <ok.rikuku@icloud.com>
2026-04-15 12:37:57 +0900

    ステージクラス追加

---
+++ Src/Object/Stage/Stage.cpp
@@ -0,0 +1,31 @@
+#include <DxLib.h>
+#include "Stage.h"
+
+Stage::Stage()
+{
+}
+
+Stage::~Stage()
+{
+}
+
+void Stage::Init(void)
+{
+	// ステージモデルの読み込み
+	//modelId_ = MV1LoadModel()
+}
+
+void Stage::Update(void)
+{
+	// ステージの更新処理が必要な場合はここに追加
+}
+
+void Stage::Draw(void)
+{
+	
+}
+
+void Stage::Release(void)
+{
+	
+}
