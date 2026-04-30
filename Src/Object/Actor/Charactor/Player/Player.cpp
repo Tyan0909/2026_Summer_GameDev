ecb970e651533ed9b3f55bac2efccde90c96f456
ASOST\2416071 <ok.rikuku@icloud.com>
2026-04-30 12:09:26 +0900

    いろいろ追加 何か実行する場合は、Playerクラスを除外してください。 アニメーションコントローラーの関係上、例外スローが発生します

--- Src/Object/Actor/Charactor/Player/Player.cpp
+++ Src/Object/Actor/Charactor/Player/Player.cpp
@@ -1,61 +1,253 @@
-#include "Player.h"
-#include "../../../../Manager/ResourceManager.h"
-#include "../../../../Object/Collider/ColliderModel.h"
+#include <string>
+#include "../../../../Application.h"
 #include "../../../../Utility/AsoUtility.h"
+#include "../../../../Manager/InputManager.h"
+#include "../../../../Manager/SceneManager.h"
+#include "../../../../Manager/ResourceManager.h"
+#include "../../../../Manager/Camera.h"
+#include "../../../Common/AnimationController.h"
+#include "../../../Common/Capsule.h"
+#include "Player.h"
 
 Player::Player(void)
-	:
-	ActorBase()
 {
+	animCtrl_ = nullptr;
+
+	// デバッグで初期は歩き
+	state_ = STATE::WALK;
+
+	// この辺は後で定数化する
+	speed_ = 0.0f;
+	moveDir_ = VGet(0.0f, 0.0f, 0.0f);
+	movePow_ = VGet(0.0f, 0.0f, 0.0f);
+	movedPos_ = VGet(0.0f, 0.0f, 0.0f);
+
+	playerRotY_ = Quaternion();
+	goalQuaRot_ = Quaternion();
+	stepRotTime_ = 0.0f;
+
+	jumpPow_ = VGet(0.0f, 0.0f, 0.0f);
+	isJump_ = false;
+	stepJump_ = 0.0f;
+
+	// 衝突判定
+	gravHitPosDown_ = VGet(0.0f, 0.0f, 0.0f);
+	gravHitPosUp_ = VGet(0.0f, 0.0f, 0.0f);
+
+	imgShadow_ = -1;
+
+	capsule_ = nullptr;
 }
 
 Player::~Player(void)
 {
+
+}
+
+void Player::Init(void)
+{
+	// モデルの基本設定
+	transform_.SetModel(resMng_.LoadModelDuplicate(ResourceManager::SRC::PLAYER));
+	transform_.scl = VGet(1.0f, 1.0f, 1.0f);
+	transform_.pos = VGet(0.0f, 100.0f, 0.0f);
+	transform_.quaRot = Quaternion();
+	transform_.quaRotLocal = 
+		Quaternion::Euler({ 0.0f,AsoUtility::Deg2RadF(180.0f),0.0f });
+	transform_.Update();
+
+	// アニメーション
+	InitAnimation();
+
+	// カプセルコライダー
+	capsule_ = std::make_unique<Capsule>(transform_);
+	capsule_ ->SetLocalPosTop(VGet(0.0f, 100.0f, 0.0f));
+	capsule_->SetLocalPosDown(VGet(0.0f, 30.0f, 0.0f));
+	capsule_->SetRadius(20.0f);
+
+	// 丸影 後で実装
+	imgShadow_ = -1;
+
+	// 初期状態
+	ChangeState(STATE::IDLE);
 }
 
 void Player::Update(void)
 {
-}
-
-void Player::InitLoad(void)
-{
-	transform_.SetModel(
-		resMng_.LoadModelDuplicate(ResourceManager::SRC::PLAYER));
-
-	// 描画されているかチェック
-	if (transform_.modelId == -1)
+	// 状態遷移
+	switch (state_)
 	{
-		// ロード失敗
-		return;
+	case STATE::IDLE:
+		UpdateIdle();
+		break;
+	case STATE::WALK:
+		UpdateWalk();
+		break;
+	case STATE::TAKE:
+		UpdateTake();
+		break;
+	case STATE::JUMP:
+		UpdateJump();
+		break;
+	default:
+		break;
 	}
-}
 
-void Player::InitTransform(void)
-{
-	transform_.scl = { 0.01f,0.01f,0.01f };
-	transform_.quaRot = Quaternion::Identity();
-	transform_.quaRotLocal = Quaternion::Identity();
-	transform_.pos = INIT_POS;
+	// モデルの基本情報更新
 	transform_.Update();
+
+	// アニメーション更新
+	animCtrl_->Update();
 }
 
-void Player::InitCollider(void)
+void Player::Draw(void)
 {
-	// DxLib側の衝突判定をセットアップ
-	MV1SetupCollInfo(transform_.modelId);
-	// モデルのコライダー
-	ColliderModel* colModel =
-		new ColliderModel(ColliderBase::TAG::STAGE, &transform_);
+	// 描画順は要注意
 
-	ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::MODEL), colModel);
+	// 丸影の描画
+	DrawShadow();
 
+	// モデルの描画
+	MV1DrawModel(transform_.modelId);
+}
+
+void Player::AddCollider(Collider* collider)
+{
+	colliders_.push_back(collider);
+}
+
+void Player::ClearCollider(void)
+{
+	colliders_.clear();
+}
+
+const Capsule* Player::GetCapsule(void) const
+{
+	return capsule_.get();
 }
 
 void Player::InitAnimation(void)
 {
+	// 挙動モデルが実装でき次第
+	/*animCtrl_ = std::make_unique<AnimationController>(transform_.modelId);
+	animCtrl_->AddInFbx(static_cast<int>(ANIM_TYPE::IDLE), 1.0f, 0);
+	animCtrl_->AddInFbx(static_cast<int>(ANIM_TYPE::WALK), 1.0f, 1);
+	animCtrl_->AddInFbx(static_cast<int>(ANIM_TYPE::TAKE), 1.0f, 2);
+	animCtrl_->AddInFbx(static_cast<int>(ANIM_TYPE::JUMP), 1.0f, 3);*/
 }
 
-void Player::InitPost(void)
+void Player::ChangeState(STATE state)
 {
+	if (state_ == state)
+	{
+		ChangeStateNone();
+		return;
+	}
+	state_ = state;
+	switch (state_)
+	{
+	case STATE::IDLE:
+		ChangeStateIdle();
+		break;
+	case STATE::WALK:
+		ChangeStateWalk();
+		break;
+	case STATE::TAKE:
+		ChangeStateTake();
+		break;
+	case STATE::JUMP:
+		ChangeStateJump();
+		break;
+	default:
+		break;
+	}
+}
+
+void Player::ChangeStateNone(void)
+{
+	// 状態遷移なし
+}
+
+void Player::ChangeStateIdle(void)
+{
+	// FBXファイルがない状態で実行すると例外スローが発生する
+	// 待機状態
+	//animCtrl_->Play(static_cast<int>(ANIM_TYPE::IDLE));
+}
+
+void Player::ChangeStateWalk(void)
+{
+	// 歩き状態
+
+}
+
+void Player::ChangeStateTake(void)
+{
+	// 写真撮影状態
+}
+
+void Player::ChangeStateJump(void)
+{
+	// ジャンプ状態
+	// 多分Walkと状態遷移統合すると思う
+}
+
+void Player::UpdateNone(void)
+{
+	// 状態遷移なし
+}
+
+void Player::UpdateIdle(void)
+{
+	// 待機状態
+}
+
+void Player::UpdateWalk(void)
+{
+	// 移動処理
+	ProcessMove();
+
+	// ジャンプ処理
+	ProcessJump();
+
+	// 移動方向に応じた回転
+	Rotate();
+
+	// 重力による移動量
+	CalcGravityPow();
+
+	// 衝突判定
+	Collision();
+
+	// 回転させる
+	transform_.quaRot = playerRotY_;
+}
+
+void Player::UpdateTake(void)
+{
+	// 写真撮影状態
+}
+
+void Player::UpdateJump(void)
+{
+	// ジャンプ状態
+	/*ProcessMove();
+	ProcessJump();
+	Collision();
+	Rotate();*/
+}
+
+void Player::DrawShadow(void)
+{
+	// 丸影の描画
+
+	// 余裕があれば
+}
+
+void Player::ProcessMove(void)
+{
+	// 移動処理
+
+	// InputManagerを取得
+	auto& 
 }
 
--- Src/Object/Actor/Charactor/Player/Player.h
+++ Src/Object/Actor/Charactor/Player/Player.h
@@ -1,25 +1,43 @@
 #pragma once
-#include <DxLib.h>
+#include <map>
 #include <vector>
-#include <string>
+#include <DxLib.h>
+#include <memory>
 #include "../../ActorBase.h"
-#include "../../../../Object/Collider/ColliderModel.h"
 
-class ResourceManager;
+class AnimationController;
+class Collider;
+class Capsule;
 
 class Player : public ActorBase
 {
-	
 public:
 
-	// 定数
-	static constexpr float GRAVITY = 0.5f;		// 重力
+	// 定数宣言
+	// 速度
 
-	// 衝突判定種別
-	enum class COLLIDER_TYPE
+	// 回転
+
+	// ジャンプ力
+
+	// ジャンプ受付時間
+
+	// モデルの状態
+	enum class STATE
 	{
-		MODEL,		// モデル
-		MAX,
+		IDLE,	// 待機
+		WALK,	// 歩き
+		TAKE,	// 写真撮影
+		JUMP,	// ジャンプ
+	};
+
+	// アニメーションの状態
+	enum class ANIM_TYPE
+	{
+		IDLE,	// 待機
+		WALK,	// 歩き
+		TAKE,	// 写真撮影
+		JUMP,	// ジャンプ
 	};
 
 	// コンストラクタ
@@ -28,35 +46,100 @@ public:
 	// デストラクタ
 	~Player(void);
 
-	// 更新
+	// 基本処理
+	void Init(void) override;
 	void Update(void) override;
-protected:
+	void Draw(void) override;
 
-	// リソースロード
-	void InitLoad(void) override;
+	// 衝突判定に必要なコライダー制御
+	void AddCollider(Collider* collider);
+	void ClearCollider(void);
 
-	// 大きさ、回転、座標の初期化
-	void InitTransform(void) override;
-
-	// 衝突判定の初期化
-	void InitCollider(void) override;
-
-	// アニメーションの初期化
-	void InitAnimation(void) override;
-
-	// 初期化後の個別処理
-	void InitPost(void) override;
-
+	// 衝突用カプセルを取得
+	const Capsule* GetCapsule(void) const;
 
 private:
 
-	// 定数
+	// アニメーション制御
+	std::unique_ptr<AnimationController> animCtrl_;
 
-	// 座標の衝突判定の初期座標
-	static constexpr VECTOR INIT_POS = { 0.0f, 400.0f, 0.0f };
+	// 状態遷移
+	STATE state_;
 
+	// 移動スピード
+	float speed_;
 
+	// 移動方向
+	VECTOR moveDir_;
 
+	// 移動量
+	VECTOR movePow_;
 
+	// 移動後の位置
+	VECTOR movedPos_;
+
+	// 回転
+	Quaternion playerRotY_;
+	Quaternion goalQuaRot_;
+	float stepRotTime_;
+
+	// ジャンプ力
+	VECTOR jumpPow_;
+
+	// ジャンプ受付時間
+	float stepJump_;
+
+	// ジャンプ判定
+	bool isJump_;
+
+	// 衝突判定に必要なコライダー
+	std::vector<Collider*> colliders_;
+	std::unique_ptr<Capsule> capsule_;
+
+	// 衝突判定
+	VECTOR gravHitPosDown_;
+	VECTOR gravHitPosUp_;
+
+	// 丸影
+	int imgShadow_;
+
+	// アニメーションの初期化
+	void InitAnimation(void);
+
+	// 状態遷移
+	void ChangeState(STATE state);
+	void ChangeStateNone(void);		//	状態遷移なし
+	void ChangeStateIdle(void);		//	待機状態
+	void ChangeStateWalk(void);		//	歩き状態
+	void ChangeStateTake(void);		//	写真撮影状態
+	void ChangeStateJump(void);		//	ジャンプ状態
+
+	// 更新
+	void UpdateNone(void);		//	状態遷移なし
+	void UpdateIdle(void);		//	待機状態
+	void UpdateWalk(void);		//	歩き状態
+	void UpdateTake(void);		//	写真撮影状態
+	void UpdateJump(void);		//	ジャンプ状態
+
+	// 描画
+	void DrawShadow(void);		//	丸影の描画
+
+	// 操作系
+	void ProcessMove(void);		//	移動処理
+	void ProcessJump(void);		//	ジャンプ処理
+
+	// 回転
+	void SetGoalRotateion(double rotRad);
+	void Rotate(void);
+
+	// 衝突判定
+	void Collision(void);
+	void CollisionGravity(void);	// 重力による衝突判定
+	void CollisionCapsule(void);	// カプセルによる衝突判定
+
+	// 移動量の計算
+	void CalcGravityPow(void);	// 重力による移動量の計算
+	
+	// 着地終了
+	bool IsEndlanding(void);
 };
-
